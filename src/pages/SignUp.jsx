00b8b208d46d29e81796f42de19591fded1c62ff
OarisaR <orebayet@gmail.com>
2025-11-12 20:51:25 +0600

    fix: Refactor SignUp component for better readability and formatting

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,13 +1,12 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth , db } from "../config/firebase";
+import { auth, db } from "../config/firebase";
 import { doc, setDoc } from "firebase/firestore";
- // your Firestore config
+// your Firestore config
 import "./SignUp.css";
 export default function SignUp() {
-
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -52,16 +51,20 @@ export default function SignUp() {
     setError("");
 
     try {
-      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
       const user = userCredential.user;
       console.log("User registered:", user);
-      await setDoc(doc(db,"users",user.uid),{
+      await setDoc(doc(db, "users", user.uid), {
         name,
         email,
         education,
         experience,
         careerTrack,
-        createdAt: new Date()
+        createdAt: new Date(),
       });
       // You can also store additional info (name, education, etc.) in Firestore here
       setName("");
